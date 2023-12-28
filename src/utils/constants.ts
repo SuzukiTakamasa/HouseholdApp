2018b6f5eae208ff12f1e6aea33706346303cc98
SuzukiTakamasa <69415488+SuzukiTakamasa@users.noreply.github.com>
2023-12-29 02:15:30 +0900

    mod constants and firestore

---
+++ src/utils/constants.ts
@@ -0,0 +1,21 @@
+export interface Household {
+    id?: string,
+    year?: number,
+    month?: number,
+    item: string,
+    amount: number,
+    isDefault: boolean,
+    version: number
+}
+
+export interface User {
+    id?: string,
+    emailAddress?: string,
+    sessionId?: string,
+    isOwner: boolean,
+    verson: number
+}
+
+export type Collection = "household" | "user"
+
+export type Condition = (string | number | boolean)[]
--- src/utils/firestore.ts
+++ src/utils/firestore.ts
@@ -1,4 +1,5 @@
 import firestore from '@react-native-firebase/firestore'
+import { Household, User, Collection } from './constants'
 
 export default class FirestoreHandler {
     private db;
@@ -6,15 +7,27 @@ export default class FirestoreHandler {
         this.db = firestore()
     }
 
-    add(collection: string, data: object) {
+    add(collection: Collection, data: Household | User) {
         this.db.collection(collection).add(data)
         .then((doc) => {console.log("データの追加に成功しました。")})
         .catch((e) => {console.log("データの追加に失敗しました。")})
     }
 
-    get(collection: string, ...condition: any) {
-        const query = condition ? this.db.collection(collection).where(condition[0], condition[1], condition[2]).get() : this.db.collection(collection).get()
-        .then((doc) => {console.log("データの参照に成功しました。")})
+    get(collection: Collection, ...condition: any) {
+        const query: any = condition ? this.db.collection(collection).where(condition[0], condition[1], condition[2]).get() : this.db.collection(collection).get()
+        .then((doc) => { return query.data() })
         .catch((e) => {console.log("データの参照に失敗しました。")})
+    }
+
+    update(collection: Collection, data: Household | User, docId: string) {
+        this.db.collection(collection).doc(docId).update(data)
+        .then((doc) => {console.log("データの更新に成功しました。")})
+        .catch((e) => {console.log("データの参照に失敗しました。")})
+    }
+
+    delete(collection: Collection, docId: string) {
+        this.db.collection(collection).doc(docId).delete()
+        .then((doc) => {console.log("データの削除に成功しました。")})
+        .catch((e) => {console.log("データの削除に失敗しました。")})
     }
 }
