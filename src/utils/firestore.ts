000bea8543f8528c4e104fa231686944030d3a90
SuzukiTakamasa <69415488+SuzukiTakamasa@users.noreply.github.com>
2023-12-31 13:08:00 +0900

    mod firestore

--- src/utils/firestore.ts
+++ src/utils/firestore.ts
@@ -1,22 +1,29 @@
-import firestore from '@react-native-firebase/firestore'
+import firestore, { FirebaseFirestoreTypes } from '@react-native-firebase/firestore'
 import { Household, User, Collection } from './constants'
 
 export default class FirestoreHandler {
-    private db: any
+    private db: FirebaseFirestoreTypes.Module
     constructor() {
-        this.db = __DEV__ ? firestore().useEmulator('localhost', 8080) : firestore()
+        this.db = firestore()
+        if (__DEV__) this.db.useEmulator('localhost', 8080)
+    }
+
+    async get(collection: Collection, ...condition: [string, FirebaseFirestoreTypes.WhereFilterOp, any]) {
+        try {
+            const query = condition.length
+            ? await this.db.collection(collection).where(...condition).get()
+            : await this.db.collection(collection).get()
+            return query.docs.map(query => query.data())
+        } catch (e) {
+            console.log("データの取得に失敗しました。")
+            throw e
+        }
     }
 
     add(collection: Collection, data: Household | User) {
         this.db.collection(collection).add(data)
         .then(() => {console.log("データの追加に成功しました。")})
         .catch(() => {console.log("データの追加に失敗しました。")})
-    }
-
-    get(collection: Collection, ...condition: any) {
-        const query: any = condition ? this.db.collection(collection).where(condition[0], condition[1], condition[2]).get() : this.db.collection(collection).get()
-        .then(() => { return query.data() })
-        .catch(() => {console.log("データの参照に失敗しました。")})
     }
 
     update(collection: Collection, data: Household | User, docId: string) {
